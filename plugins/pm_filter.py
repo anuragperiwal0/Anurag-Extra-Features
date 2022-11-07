ceaf5d23491112f5d2ad07c2202a2d65b0c3ac53
anuragperiwal0 <116804267+anuragperiwal0@users.noreply.github.com>
2022-11-07 18:04:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -11,7 +11,7 @@ from database.connections_mdb import active_connection, all_connections, delete_
     make_inactive
 from info import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
